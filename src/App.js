60d3372c0ec5f55927459dae1a9bbf2fd3adefbe
sboursen <dev.sboursen@gmail.com>
2022-03-15 11:56:44 +0100

    Build the header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="App-logo">
+          <img
+            className="App-logo-img"
+            src={logo}
+            alt="logo"
+          />
+          <span>Math Magicians</span>
+        </h1>
+        <ul className="App-navigation">
+          <li>
+            <a href="./">Home</a>
+          </li>
+          <li>|</li>
+          <li>
+            <a href="./">Calculator</a>
+          </li>
+          <li>|</li>
+          <li>
+            <a href="./">Quotes</a>
+          </li>
+        </ul>
       </header>
     </div>
   );
