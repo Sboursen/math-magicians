34feb55da28de2856627d6e9a401e90256b77582
sboursen <dev.sboursen@gmail.com>
2022-03-15 14:52:08 +0100

    Add buttons to calculator

--- src/components/button.js
+++ src/components/button.js
@@ -6,19 +6,28 @@ export default class Button extends React.Component {
   constructor(props) {
     super(props);
     this.text = props.text;
-    this.type = props.type;
+    this.position = props.position;
+    this.className = props.className;
   }
 
   render() {
     return (
-      <button type="button" className={`${this.type}`}>
+      <button
+        type="button"
+        className={`${this.type} ${this.className}`}
+      >
         {this.text}
       </button>
     );
   }
 }
 
+Button.defaultProps = {
+  className: '',
+};
+
 Button.propTypes = {
   text: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Button from './button';
 import './calculator.css';
 
 export default class Calculator extends React.Component {
@@ -13,6 +14,29 @@ export default class Calculator extends React.Component {
           value={0}
           readOnly
         />
+        <Button position="main" text="AC" />
+        <Button position="main" text="+/-" />
+        <Button position="main" text="%" />
+        <Button position="aside" text="+" />
+        <Button position="main" text="7" />
+        <Button position="main" text="8" />
+        <Button position="main" text="9" />
+        <Button position="aside" text="&times;" />
+        <Button position="main" text="4" />
+        <Button position="main" text="5" />
+        <Button position="main" text="6" />
+        <Button position="aside" text="-" />
+        <Button position="main" text="1" />
+        <Button position="main" text="2" />
+        <Button position="main" text="3" />
+        <Button position="aside" text="+" />
+        <Button
+          position="main"
+          text="0"
+          className="wide-button"
+        />
+        <Button position="main" text="." />
+        <Button position="main" text="=" />
       </div>
     );
   }
