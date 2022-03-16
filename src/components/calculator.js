c305edab015f284880a2daa6943c407d1f9735d9
sboursen <dev.sboursen@gmail.com>
2022-03-16 13:10:42 +0100

    Link the logic to the calculator app

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,41 +2,128 @@
 import React from 'react';
 import Button from './button';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj: { total: null, next: null, operation: null },
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { obj } = this.state;
+    const newObj = {
+      ...obj,
+      ...calculate(obj, buttonName),
+    };
+    this.setState({ obj: newObj });
+  }
+
   render() {
+    const { obj } = this.state;
+    const res = obj.total || obj.next;
     return (
       <div className="calculator-wrap">
-        <input
-          type="text"
-          name="result"
-          id="res"
-          value={0}
-          readOnly
+        <div id="res">{res}</div>
+        <Button
+          position="main"
+          text="AC"
+          onButtonClick={this.handleClick}
         />
-        <Button position="main" text="AC" />
-        <Button position="main" text="+/-" />
-        <Button position="main" text="%" />
-        <Button position="aside" text="+" />
-        <Button position="main" text="7" />
-        <Button position="main" text="8" />
-        <Button position="main" text="9" />
-        <Button position="aside" text="&times;" />
-        <Button position="main" text="4" />
-        <Button position="main" text="5" />
-        <Button position="main" text="6" />
-        <Button position="aside" text="-" />
-        <Button position="main" text="1" />
-        <Button position="main" text="2" />
-        <Button position="main" text="3" />
-        <Button position="aside" text="+" />
+        <Button
+          position="main"
+          text="+/-"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="%"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="aside"
+          text="÷"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="7"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="8"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="9"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="aside"
+          text="x"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="4"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="5"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="6"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="aside"
+          text="-"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="1"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="2"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="main"
+          text="3"
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="aside"
+          text="+"
+          onButtonClick={this.handleClick}
+        />
         <Button
           position="main"
           text="0"
           className="wide-button"
+          onButtonClick={this.handleClick}
         />
-        <Button position="main" text="." />
-        <Button position="aside" text="=" />
+        <Button
+          position="main"
+          text="."
+          onButtonClick={this.handleClick}
+        />
+        <Button
+          position="aside"
+          text="="
+          onButtonClick={this.handleClick}
+        />
       </div>
     );
   }
