01cfb510fd680f85d3c1ddf3f9fc74432e3d4292
sboursen <dev.sboursen@gmail.com>
2022-03-17 14:00:29 +0100

    Refractor App.js to use function-based component

--- src/App.js
+++ src/App.js
@@ -3,37 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import CalculatorBody from './components/calculatorBody';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-logo">
-            <img
-              className="App-logo-img"
-              src={logo}
-              alt="logo"
-            />
-            <span>Math Magicians</span>
-          </h1>
-          <ul className="App-navigation">
-            <li>
-              <a href="./">Home</a>
-            </li>
-            <li>|</li>
-            <li>
-              <a href="./">Calculator</a>
-            </li>
-            <li>|</li>
-            <li>
-              <a href="./">Quotes</a>
-            </li>
-          </ul>
-        </header>
-        <CalculatorBody />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-logo">
+          <img
+            className="App-logo-img"
+            src={logo}
+            alt="logo"
+          />
+          <span>Math Magicians</span>
+        </h1>
+        <ul className="App-navigation">
+          <li>
+            <a href="./">Home</a>
+          </li>
+          <li>|</li>
+          <li>
+            <a href="./">Calculator</a>
+          </li>
+          <li>|</li>
+          <li>
+            <a href="./">Quotes</a>
+          </li>
+        </ul>
+      </header>
+      <CalculatorBody />
+    </div>
+  );
 }
-
-export default App;
