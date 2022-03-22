b39dcb7df7fb1802951eb9d78261ca27be56ca2f
sboursen <dev.sboursen@gmail.com>
2022-03-22 04:18:15 +0100

    Add basic quotes page

---
+++ src/routes/quotes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Quotes() {
+  return (
+    <h2>
+      Make everything as simple as possible, but not simpler
+      - Albert Einstein
+    </h2>
+  );
+}
