c60f30b0179dfaf834d00d8000424744173038ee
sboursen <dev.sboursen@gmail.com>
2022-03-22 04:21:50 +0100

    Add basic home page

---
+++ src/routes/home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main>
+      <h2>Welcome to our page!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Viverra vitae congue eu
+        consequat. Convallis convallis tellus id interdum
+        velit laoreet. Lacus vestibulum sed arcu non odio
+        euismod lacinia. Feugiat pretium nibh ipsum
+        consequat nisl. Sit amet consectetur adipiscing elit
+        ut. Sed tempus urna et pharetra. Gravida cum sociis
+        natoque penatibus et. Id donec ultrices tincidunt
+        arcu non sodales neque. Ac turpis egestas maecenas
+        pharetra convallis posuere morbi. Massa tincidunt
+        dui ut ornare lectus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Viverra vitae congue eu
+        consequat. Convallis convallis tellus id interdum
+        velit laoreet. Lacus vestibulum sed arcu non odio
+        euismod lacinia. Feugiat pretium nibh ipsum
+        consequat nisl. Sit amet consectetur adipiscing elit
+        ut. Sed tempus urna et pharetra. Gravida cum sociis
+        natoque penatibus et. Id donec ultrices tincidunt
+        arcu non sodales neque. Ac turpis egestas maecenas
+        pharetra convallis posuere morbi. Massa tincidunt
+        dui ut ornare lectus.
+      </p>
+    </main>
+  );
+}
