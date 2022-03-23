bcbe5cd4cd0591816c17788f377cd1f0e223f4e7
sboursen <dev.sboursen@gmail.com>
2022-03-23 15:26:51 +0100

    Test the calculate function

--- src/App.testdffs.js
+++ src/App.testdffs.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Math Magicians/i);
+  // const linkElement = screen.getByDisplayValue(/Math Magicians/i);
   expect(linkElement).toBeInTheDocument();
 });
