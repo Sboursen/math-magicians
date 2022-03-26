79bff0d8fada970f8e828eb5b778c112ad9a1f66
sboursen <dev.sboursen@gmail.com>
2022-03-26 13:01:20 +0100

    Save notes to local storage

--- src/components/notebook/notebook.js
+++ src/components/notebook/notebook.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import './notebook.scss';
 
-export default class Notebook extends React.PureComponent {
+const getNotesFromStorage = () => {
+  const key = 'notes';
+  const savedNotes = localStorage.getItem(key);
+  if (savedNotes) {
+    return JSON.parse(savedNotes);
+  }
+  localStorage.setItem(key, '');
+  return '';
+};
+
+const setNotesToStorage = (newNotes) => {
+  const key = 'notes';
+  localStorage.setItem(key, JSON.stringify(newNotes));
+};
+
+export default class Notebook extends React.Component {
+  constructor() {
+    super();
+    this.state = { notes: '' };
+  }
+
+  componentDidMount() {
+    this.setState({ notes: getNotesFromStorage() });
+  }
+
+  handleChange = (e) => {
+    const newNotes = e.target.value;
+    setNotesToStorage(newNotes);
+    this.setState({ notes: newNotes });
+  };
+
   render() {
+    const { notes } = this.state;
     return (
       <section
         className="notebook-container"
@@ -15,6 +46,9 @@ export default class Notebook extends React.PureComponent {
             name="notebook"
             id="notebook"
             placeholder="Write your notes here"
+            onChange={this.handleChange}
+            onBlur={this.handleChange}
+            value={notes}
           />
         </label>
       </section>
